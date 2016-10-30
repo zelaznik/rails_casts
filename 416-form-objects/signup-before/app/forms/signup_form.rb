d575b59899f7ffeeb786909444db7ad96b329e59
Steve Zelaznik <zelaznik@yahoo.com>
2016-10-30 16:21:28 -0500

    416 got signup form to work.

--- 416-form-objects/signup-before/app/forms/signup_form.rb
+++ 416-form-objects/signup-before/app/forms/signup_form.rb
@@ -1,9 +1,45 @@
-module Api::SignupForm
+class SignupForm
   extend  ActiveModel::Naming
   include ActiveModel::Conversion
   include ActiveModel::Validations
 
   def persisted?
     false
+  end
+
+  validate :verify_unique_username
+  validates_presence_of :username
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
+  validates_length_of :password, minimum: 6
+
+  delegate :username, :email, :password, :password_confirmation, to: :user
+  delegate :twitter_name, :github_name, :bio, to: :profile
+
+  def user
+    @user ||= User.new
+  end
+
+  def profile
+    @profile ||= user.build_profile
+  end
+
+  def subscribed
+    user.subscribed_at
+  end
+
+  def subscribed=(checkbox)
+    user.subscribed_at = Time.zone.now if checkbox == '1'
+  end
+
+  def generate_token
+    begin
+      user.token = SecureRandom.hex
+    end while User.exists?(token: token)
+  end
+
+  def verify_unique_username
+    if User.exists? username: username
+      errors.add :username, "has already been taken"
+    end
   end
 end
--- 416-form-objects/signup-before/lib/generators/form/form_generator.rb
+++ 416-form-objects/signup-before/lib/generators/form/form_generator.rb
@@ -3,7 +3,7 @@ class FormGenerator < Rails::Generators::NamedBase
 
   def create_form_file
     create_file "app/forms/#{file_name}_form.rb", <<-FILE
-module #{class_name}Form
+class #{class_name}Form
   extend  ActiveModel::Naming
   include ActiveModel::Conversion
   include ActiveModel::Validations
